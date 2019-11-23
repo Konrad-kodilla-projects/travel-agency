495d94fc6fa7e5082a7f7fb6e95ffd2b15f71610
Konrad <konrad.bonder@gmail.com>
2019-11-23 13:45:20 +0100

    Add filters in tripRedux.js

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -1,41 +1,50 @@
 /* SELECTORS */
 
-export const getAllTrips = ({trips}) => trips;
+export const getAllTrips = ({ trips }) => trips;
 
-export const getFilteredTrips = ({trips, filters}) => {
+export const getFilteredTrips = ({ trips, filters }) => {
   let output = trips;
+  const { searchPhrase, tags } = filters;
 
   // filter by search phrase
-  if(filters.searchPhrase){
-    const pattern = new RegExp(filters.searchPhrase, 'i');
+  if (searchPhrase) {
+    const pattern = new RegExp(searchPhrase, 'i');
     output = output.filter(trip => pattern.test(trip.name));
   }
 
   // TODO - filter by duration
+  const { to, from } = filters.duration;
+  output = output.filter(trip => trip.days >= from && trip.days <= to);
 
   // TODO - filter by tags
+  tags.length > 0
+    ? (output = output.filter(trip =>
+      tags.every(tag => trip.tags.includes(tag))
+    ))
+    : null;
 
   // TODO - sort by cost descending (most expensive goes first)
-
+  const toNum = str => Number(str.replace('$', '').replace(',',''));
+  output.sort((a, b) => toNum(b.cost) - toNum(a.cost));
   return output;
 };
 
-export const getTripById = ({trips}, tripId) => {
+export const getTripById = ({ trips }, tripId) => {
   const filtered = trips.filter(trip => trip.id === tripId);
 
   // TODO - filter trips by tripId
 
   console.log('filtering trips by tripId:', tripId, filtered);
-  return filtered.length ? filtered[0] : {error: true};
+  return filtered.length ? filtered[0] : { error: true };
 };
 
-export const getTripsForCountry = ({trips}, countryCode) => {
+export const getTripsForCountry = ({ trips }, countryCode) => {
   const filtered = trips.filter(trip => trip.country.code === countryCode);
 
   // TODO - filter trips by countryCode
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
-  return filtered.length ? filtered : [{error: true}];
+  return filtered.length ? filtered : [{ error: true }];
 };
 
 /* ACTIONS */
