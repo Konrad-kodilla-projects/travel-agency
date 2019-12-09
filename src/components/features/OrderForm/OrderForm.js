a8336881c4bd7dd29df4b2f18e7867cb387bc4a8
Konrad <konrad.bonder@gmail.com>
2019-12-09 12:29:05 +0100

    Add json-server

--- src/components/features/OrderForm/OrderForm.js
+++ src/components/features/OrderForm/OrderForm.js
@@ -4,10 +4,41 @@ import { Row, Col } from 'react-flexbox-grid';
 
 import OrderSummary from '../OrderSummary/OrderSummary';
 import OrderOption from '../OrderOption/OrderOption';
+import Button from '../../common/Button/Button';
 
 import pricing from '../../../data/pricing.json';
+import {calculateTotal} from '../../../utils/calculateTotal';
+import {formatPrice} from '../../../utils/formatPrice';
+import settings from '../../../data/settings';
 
-const OrderForm = ({ tripCost, options, setOrderOption }) => (
+const sendOrder = (options, tripCost, id) => {
+  const totalCost = formatPrice(calculateTotal(tripCost, options));
+  const {url, endpoint: {orders} } = settings.db;
+
+  const payload = {
+    ...options,
+    totalCost,
+    tripID: id,
+  };
+  
+  const fetchOptions = {
+    cache: 'no-cache',
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(payload),
+  };
+
+  fetch(url + '/' + orders, fetchOptions)
+    .then(function(response){
+      return response.json();
+    }).then(function(parsedResponse){
+      console.log('parsedResponse', parsedResponse);
+    });
+};
+
+const OrderForm = ({ tripCost, options, setOrderOption, id }) => (
   <Row>
     {pricing.map(option => (
       <Col md={4} key={option.id}>
@@ -22,6 +53,7 @@ const OrderForm = ({ tripCost, options, setOrderOption }) => (
     <Col xs={12}>
       <OrderSummary tripCost={tripCost} options={options} />
     </Col>
+    <Button onClick={() => sendOrder(options, tripCost, id)}>Order now!</Button>
   </Row>
 );
 
@@ -29,6 +61,7 @@ OrderForm.propTypes = {
   tripCost: PropTypes.string,
   options: PropTypes.object,
   setOrderOption: PropTypes.func.isRequired,
+  id: PropTypes.string.isRequired,
 };
 
 export default OrderForm;
